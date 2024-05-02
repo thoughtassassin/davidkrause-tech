95dd3e637cb4b963cfdc135eb8a434c9892ba115
David <david@onerootelement.com>
2024-05-02 14:26:43 -0600

    Adjust spacing

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Nav() {
   return (
-    <nav className="flex justify-start gap-10 items-center p-10 border-b-2 border-gray-800">
+    <nav className="flex justify-start gap-10 items-center px-24 py-5 border-b-2 border-gray-800">
       <Link href="/">Journey</Link>
       <Link href="/projects">Projects</Link>
     </nav>
