10619e103c3a9cb2037f13615971099eb0aa8c2e
David <david@onerootelement.com>
2024-05-02 19:39:39 -0600

    Fix mobile text color

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Journey from "@/app/components/Journey";
 
 export default function Home() {
   return (
-    <main className="bg-black flex min-h-screen flex-col items-center justify-between pt-32 p-24 md:pt-24">
+    <main className="bg-black text-white flex min-h-screen flex-col items-center justify-between pt-32 p-24 md:pt-24">
       <Journey />
     </main>
   );
